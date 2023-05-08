c9cee93d5cdff0dfc0893c8abf379f8bb2e75c4d
Roman Leshko <14238237+romanleshko@users.noreply.github.com>
2023-05-08 15:02:08 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,68 @@
+window.onload = function() {
+    getUptime(new Date("January 20, 2020"))
+    updateDate();
+    
+    animateGraphLine("loadlevel");
+    animateGraphLine("memlevel");
+    animateGraphLine("intrlevel");
+    animateGraphLine("syscalllevel");
+    animateGraphLine("contextlevel");
+};
+
+function getUptime(target) {
+    const now = new Date();
+    const diff = now.getTime() - target.getTime();
+
+    const year = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+    const month = Math.floor((diff % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * (365.25 / 12)));
+    const day = Math.floor((diff % (1000 * 60 * 60 * 24 * (365.25 / 12))) / (1000 * 60 * 60 * 24));
+
+    let uptime = `${year} year${year !== 1 ? "s" : ""}`;
+    if (month !== 0) {
+        uptime += ` ${month} month${month !== 1 ? "s" : ""}`;
+    }
+    if (day !== 0) {
+        uptime += ` ${day} day${day !== 1 ? "s" : ""}`;
+    }
+
+    document.getElementById("uptime").innerText = uptime;
+}
+function updateDate() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = padZero(date.getMonth() + 1);
+    const day = padZero(date.getDate());
+    const dayOfWeek = getDayOfWeek(date.getDay());
+    const hours = padZero(date.getHours());
+    const minutes = padZero(date.getMinutes());
+    const seconds = padZero(date.getSeconds());
+
+    const now = `${year}/${month}/${day} ${dayOfWeek} ${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").innerText = now;
+
+    setTimeout(updateDate, 1000);
+}
+
+function padZero(num) {
+    return num < 10 ? `0${num}` : num;
+}
+
+function getDayOfWeek(day) {
+    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return daysOfWeek[day];
+}
+
+function animateGraphLine(elementId) {
+  let width = 0;
+  const element = document.getElementById(elementId);
+
+  const intervalId = setInterval(() => {
+    if (width >= 100) {
+      clearInterval(intervalId);
+      return;
+    }
+
+    width += 1;
+    element.style.width = `${width}%`;
+  }, 1000);
+}
